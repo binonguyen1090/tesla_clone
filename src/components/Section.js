fac6742f4db4edecb063c90e5cd01acead6286e7
Bino <binonguyen1090@gmail.com>
2021-10-05 00:00:11 -0500

    add button group component

--- src/components/Section.js
+++ src/components/Section.js
@@ -7,6 +7,14 @@ function Section() {
                 <h1>Model S</h1>
                 <p>Order Online for Touchless Delivery</p>
             </ItemText>
+            <ButtonGroup>
+                <LeftButton>
+                    Custom Order
+                </LeftButton>
+                <RightButton>
+                    Existing Inventory
+                </RightButton>
+            </ButtonGroup>
         </Wrap>
     )
 }
@@ -20,8 +28,34 @@ const Wrap = styled.div`
     background-size: cover;
     background-position: center;
     background-repeat: no-repeat;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+
 `
 const ItemText = styled.div`
     padding-top: 15vh;
     text-align:center;
+`
+const ButtonGroup = styled.div`
+    display: flex;
+   margin-bottom: 30px;
+`
+const LeftButton = styled.div`
+    background-color: rgba(23, 26, 32, 0.8);
+    height: 40px;
+    width: 256px;
+    color: white;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 100px;
+    opacity: 0.85;
+    text-transform: uppercase;
+    font-size: 12px;
+    cursor: pointer;
+`
+const RightButton = styled(LeftButton)`
+    
 `
