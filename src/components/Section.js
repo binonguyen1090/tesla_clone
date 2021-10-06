e13db7989ff26a68b1ff18eb9ddcb4dd565504ca
Bino <binonguyen1090@gmail.com>
2021-10-05 22:34:42 -0500

    using props to create dynamic Section component

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,19 +1,19 @@
 import React from 'react'
 import styled from 'styled-components'
-function Section() {
+function Section({title,description,backgroundImg,leftBtnText,rightBtnText}) {
     return (
-        <Wrap>
+        <Wrap bgImage={backgroundImg}>
             <ItemText>
-                <h1>Model S</h1>
-                <p>Order Online for Touchless Delivery</p>
+                <h1>{title}</h1>
+                <p>{description}</p>
             </ItemText>
             <Buttons>
                 <ButtonGroup>
                     <LeftButton>
-                        Custom Order
+                        {leftBtnText}
                     </LeftButton>
                     <RightButton>
-                        Existing Inventory
+                        {rightBtnText}
                     </RightButton>
                 </ButtonGroup>
                 <DownArrow src="/images/down-arrow.svg"/>
@@ -27,7 +27,7 @@ const Wrap = styled.div`
     width: 100vw;
     height: 100vh;
     background: orange;
-    background-image: url('/images/model-s.jpg');
+    background-image: ${props => `url("/images/${props.bgImage}")`};
     background-size: cover;
     background-position: center;
     background-repeat: no-repeat;
@@ -43,7 +43,10 @@ const ItemText = styled.div`
 `
 const ButtonGroup = styled.div`
     display: flex;
-   margin-bottom: 30px;
+    margin-bottom: 30px;
+    @media (max-width: 768px) {
+        flex-direction: column;
+    }
 `
 const LeftButton = styled.div`
     background-color: rgba(23, 26, 32, 0.8);
