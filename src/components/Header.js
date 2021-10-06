0c3d3ec6a0748a6b70c587647af65913c6b539b7
Bino <binonguyen1090@gmail.com>
2021-10-05 23:09:40 -0500

    add Logo and sub-menu into Header component '

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,43 @@
 import React from 'react'
+import styled from 'styled-components'
 
 function Header() {
     return (
-        <div>
-            Header
-        </div>
+        <Container>
+            <a >
+                <img src="/images/logo.svg"/>
+            </a>
+            <Menu>
+                <p><a href="#">Model S</a></p>
+                <p><a href="#">Model Y</a></p>
+                <p><a href="#">Model 3</a></p>
+                <p><a href="#">Model X</a></p>
+            </Menu>
+        </Container>
     )
 }
 
 export default Header
+const Container = styled.div`
+    min-height: 60px;
+    position: fixed;
+    display: flex;
+    align-items: center;
+    padding: 0 20px;
+    top: 0;
+    left: 0;
+    right: 0;
+`
+const Menu = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex: 1;
+
+    p {
+        font-weight: 600;
+        text-transform: uppercase;
+        padding: 0 10px;
+        flex-wrap: nowrap;
+    }
+`
