959522a35ecba7dd0ff5a10a616a550fac807322
Bino <binonguyen1090@gmail.com>
2021-10-06 00:04:49 -0500

    add styled Hamburger Menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 import MenuIcon from '@mui/icons-material/Menu';
+import CloseIcon from '@mui/icons-material/Close';
 function Header() {
     return (
         <Container>
@@ -18,6 +19,19 @@ function Header() {
                 <a href="#">Account</a>
                 <CustomMenu />
             </RightMenu>
+            <BurgerNav>
+                <CloseWrapper>
+                    <CustomClose/>
+                </CloseWrapper>   
+                <li><a href="#">Model S</a></li>
+                <li><a href="#">Solar Roof</a></li>
+                <li><a href="#">Solar Panels</a></li>
+                <li><a href="#">Existing Inventory</a></li>
+                <li><a href="#">Used Inventory</a></li>
+                <li><a href="#">Trade-In</a></li>
+                <li><a href="#">Powerwall</a></li>
+                <li><a href="#">Find Us</a></li>
+            </BurgerNav>
         </Container>
     )
 }
@@ -33,6 +47,7 @@ const Container = styled.div`
     top: 0;
     left: 0;
     right: 0;
+    z-index: 1;
 `
 const Menu = styled.div`
     display: flex;
@@ -64,4 +79,36 @@ const RightMenu = styled.div`
 const CustomMenu = styled(MenuIcon)`
     cursor: pointer;
 
+`
+const CustomClose = styled(CloseIcon)`
+    cursor: pointer;
+
+`
+const BurgerNav = styled.div`
+    position: fixed;
+    top: 0;
+    bottom:0;
+    right:0;
+    background: white;
+    width: 300px;
+    z-index: 16;
+    list-style:none;
+    padding: 20px;
+    display: flex;
+    flex-direction: column;
+    text-align: start;
+
+    li {
+        padding: 15px 0;
+        border-bottom: 1px solid rgb(0,0,0,.2);
+
+        a {
+            font-weight: 600;
+
+        }
+    }
+`
+const CloseWrapper = styled.div`
+    display: flex;
+    justify-content: flex-end;
 `
